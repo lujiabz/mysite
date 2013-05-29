e21e60fd838623b9a3c3326f50ff0eac74106abb
gitoneman <lujiabz@gmail.com>
2013-05-29 22:15:07 +0800

    my first site

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+
+/*
+ * GET home page.
+ */
+
+module.exports = function(app){
+	app.get('/', function(req,res)
+		{
+			res.render('index', { title: '阅微笔记' });
+		});
+	app.get('/javascript', function(req,res)
+		{
+			res.render('javascript', { title: '阅微笔记/javascript' });
+		});
+	app.get('/jQuery', function(req,res)
+		{
+			res.render('jQuery', { title: '阅微笔记/jQuery' });
+		});
+	app.get('/homepages', function(req,res)
+		{
+			res.render('homepages', { title: '阅微笔记/homepages' });
+		});
+	
+  
+};
